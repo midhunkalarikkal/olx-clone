43c7fd4dfb1e474be5f2e701ad5d8f092fe18740
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-02-14 19:54:59 +0530

    Update : firebase config updated

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -10,7 +10,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REAC_APP_FIREBASE_API,
+  apiKey: process.env.REACT_APP_FIREBASE_API,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
