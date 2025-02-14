4abe6d254572e9c1990eeec7e85fe74e5c079477
midhunkalarikkal <91378594+midhunkalarikkal@users.noreply.github.com>
2025-02-14 19:11:13 +0530

    Updated

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -10,13 +10,13 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC68wP1DKgTeOMpqw8CI85wy1s1hehfFNI",
-  authDomain: "olx-clone-5630e.firebaseapp.com",
-  projectId: "olx-clone-5630e",
-  storageBucket: "olx-clone-5630e.firebasestorage.app",
-  messagingSenderId: "1091902646970",
-  appId: "1:1091902646970:web:cd3c07c868264da9073083",
-  measurementId: "G-G11LDW7CTM"
+  apiKey: process.env.REAC_APP_FIREBASE_API,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 // Initialize Firebase
